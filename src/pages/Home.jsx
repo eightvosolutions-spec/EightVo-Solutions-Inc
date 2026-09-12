18432a70d4da12df60c5ac3a9ff830d11e83270b
eightvosolutions-spec <eightvosolutions@gmail.com>
2026-09-12 10:29:39 -0400

    Remove homepage maintenance banner and its animation styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,6 @@ import React from 'react'
 import LeadershipCard from '../components/LeadershipCard'
 import NewsBanner from '../components/NewsBanner'
 import CaseStudyCard from '../components/CaseStudyCard'
-import MaintenanceBanner from '../components/MaintenanceBanner'
 import useDocumentTitle from '../utils/useDocumentTitle'
 
 const leaders = [
@@ -18,7 +17,6 @@ export default function Home(){
   useDocumentTitle('Home')
   return (
     <div className="page home">
-      <MaintenanceBanner />
       <section className="hero">
         <div className="container hero-inner">
           <div style={{maxWidth:640}}>
